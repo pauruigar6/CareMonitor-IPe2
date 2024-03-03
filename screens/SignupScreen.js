58921c84d6e5d1d9333017312f427aa2b810502a
pauruigar6 <pauruigar6@alum.us.es>
2024-03-03 21:04:52 +0100

    SignupScreen

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,12 +1,113 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+// SignupScreen.js
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import Styles from "../constants/styles";
+import Input from "../constants/input";
+import { logo } from "../constants/images";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { COLORS } from "../constants/appConfig";
+import WhiteButtom from "../components/WhiteButton";
 
-const SignupScreen = () => {
+const LoginScreen = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState("");
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleToggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
   return (
-    <View>
-      <Text>SignupScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={Styles.safeAreaView}>
+      <ScrollView style={Styles.scrollView}>
+        <Image source={logo} resizeMode="contain" style={Styles.smallLogo} />
+        <Text style={Styles.smallTitle}>Sign Up</Text>
+        <Text style={Styles.subtitle}>Start your wellness voyage.</Text>
 
-export default SignupScreen
+        <Input
+          id="name"
+          placeholder="Name"
+          value={name}
+          onInputChanged={(id, text) => setName(text)}
+        />
+        <Input
+          id="email"
+          placeholder="Email Address"
+          value={email}
+          onInputChanged={(id, text) => setEmail(text)}
+          keyboardType="email-address"
+        />
+
+        <View style={Styles.passwordContainer}>
+          <Input
+            id="password"
+            placeholder="Password"
+            value={password}
+            onInputChanged={(id, text) => setPassword(text)}
+            secureTextEntry={!showPassword}
+          />
+          <TouchableOpacity
+            onPress={handleTogglePasswordVisibility}
+            style={Styles.eyeIcon}
+          >
+            <FontAwesome5
+              name={showPassword ? "eye-slash" : "eye"}
+              size={20}
+              color={COLORS.white}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View style={Styles.passwordContainer}>
+          <Input
+            id="confirmPassword"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onInputChanged={(id, text) => setConfirmPassword(text)}
+            secureTextEntry={!showConfirmPassword}
+            passwordValue={password}
+          />
+          <TouchableOpacity
+            onPress={handleToggleConfirmPasswordVisibility}
+            style={Styles.eyeIcon}
+          >
+            <FontAwesome5
+              name={showConfirmPassword ? "eye-slash" : "eye"}
+              size={20}
+              color={COLORS.white}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <WhiteButtom
+          title="Sign Up"
+          style={Styles.whiteBtn}
+          onPress={() => navigation.navigate("Login")}
+        />
+
+        <View style={Styles.btnContainer}>
+          <Text style={Styles.subtitle}>Already have an account?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+            <Text style={Styles.btnText}> Log In</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
